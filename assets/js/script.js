aea271d445f6112dbf4279702e02248f785d85b3
Danish Shafi <danish.shafi@live.co.uk>
2023-01-27 19:20:52 +0000

    API call to city works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log('hi')
+// 1. Grab API Query URL with API key
+var API_KEY = '64b3c6a31b964dd3885bceb16d79051e';
+var API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
+
+$.ajax({
+  url: API_URL,
+  method: "GET"
+}).then(function(response) {
+console.log(response);
+});
