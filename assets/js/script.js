a412ab91e6b621eb016161c6d945a5ade03372e1
Danish Shafi <danish.shafi@live.co.uk>
2023-01-27 19:31:01 +0000

    Search button event listener and test added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
-// 1. Grab API Query URL with API key
+// DOM variables
+var searchBtn = $('#search-button');
+var searchHistory = $('#history');
+
+// API variables
 var API_KEY = '64b3c6a31b964dd3885bceb16d79051e';
 var API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
 
-$.ajax({
-  url: API_URL,
-  method: "GET"
-}).then(function(response) {
-console.log(response);
+// API call
+// $.ajax({
+//   url: API_URL,
+//   method: "GET"
+// }).then(function(response) {
+// console.log(response);
+// });
+
+// EventListener on Search Button function
+searchBtn.on("click", function(event) {
+  event.preventDefault();
+  searchHistory.append('<p>Test</p>'); // CHANGE THIS
 });
