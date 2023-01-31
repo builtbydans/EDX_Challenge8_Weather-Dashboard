06f139d541d80328ead6094f9ff23463be7ab950
Danish Shafi <danish.shafi@live.co.uk>
2023-01-31 23:37:39 +0000

    Able to display weather from user search of a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
-  // DOM variables
+  // Global DOM variables
   var searchBtn = $('#search-button');
   var searchHistory = $('#history');
-
-  // API variables
-  var API_KEY = '64b3c6a31b964dd3885bceb16d79051e';
-  var API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
-
-  // API call
-  // $.ajax({
-  //   url: API_URL,
-  //   method: "GET"
-  // }).then(function(response) {
-  // console.log(response);
-  // });
+  var weatherBox = $('#weather-box');
 
   // EventListener on Search Button function
   searchBtn.on("click", function(event) {
+    // Prevent reload of page
     event.preventDefault();
-    var searchInput = $('#search-input').val();
-    searchHistory.append(`<p class="btn btn-secondary">${searchInput}</p>`); // CHANGE THIS
+    // Grabbing cityName from input field
+    var cityName = $('#search-input').val()
+    // API variables
+    var API_KEY = '64b3c6a31b964dd3885bceb16d79051e';
+    var API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`;
+    // Adding userSearch to their history
+    searchHistory.append(`<p class="btn btn-secondary">${cityName}</p>`);
+
+    // API call and displaying of weather
+    $.ajax({
+      url: API_URL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      // Creating variables for the weatherBox
+      var todayDate = moment().format("DD/MM/YY");
+      var cityHeader = $(`<h2>${response.city.name + ' ' + todayDate}</h2>`);
+      var temp = $(`<p>Temp: ${Math.round((response.list[0].main.temp) - 273.15)}°C</p>`);
+      var wind = $(`<p>Wind: ${response.list[0].wind.speed}MPH</p>`);
+      var humidity = $(`<p>Humidity: ${response.list[0].main.humidity}%</p>`);
+      // Appending variables to the weatherBox
+      weatherBox.append(cityHeader, temp, wind, humidity);
+    });
   });
 });
