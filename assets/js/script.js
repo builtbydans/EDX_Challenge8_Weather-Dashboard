8040754e1cefd4d61966b5fe4e8b335d688059ce
Danish Shafi <danish.shafi@live.co.uk>
2023-01-31 22:55:13 +0000

    added historic text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
-// DOM variables
-var searchBtn = $('#search-button');
-var searchInput = $('#search-input');
-var searchHistory = $('#history');
+$(document).ready(function() {
+  // DOM variables
+  var searchBtn = $('#search-button');
+  var searchHistory = $('#history');
 
-// API variables
-var API_KEY = '64b3c6a31b964dd3885bceb16d79051e';
-var API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
+  // API variables
+  var API_KEY = '64b3c6a31b964dd3885bceb16d79051e';
+  var API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=${API_KEY}`;
 
-// API call
-// $.ajax({
-//   url: API_URL,
-//   method: "GET"
-// }).then(function(response) {
-// console.log(response);
-// });
+  // API call
+  // $.ajax({
+  //   url: API_URL,
+  //   method: "GET"
+  // }).then(function(response) {
+  // console.log(response);
+  // });
 
-var historicChoice = searchInput.val();
-
-// EventListener on Search Button function
-searchBtn.on("click", function(event) {
-  event.preventDefault();
-  searchHistory.append(`<p class="btn btn-secondary">${historicChoice}</p>`); // CHANGE THIS
+  // EventListener on Search Button function
+  searchBtn.on("click", function(event) {
+    event.preventDefault();
+    var searchInput = $('#search-input').val();
+    searchHistory.append(`<p class="btn btn-secondary">${searchInput}</p>`); // CHANGE THIS
+  });
 });
